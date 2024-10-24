b9066202aad594d2b543a47973c93e843a1c4494
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-10-24 14:32:07 -0300

    feat(dashboard-cards): criando os cards com informações de receita e cancelamentos

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,23 @@
+import { Helmet } from 'react-helmet-async'
+
+import { DayOrdersAmountCard } from './day-orders-amount-card'
+import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount'
+import { MonthOrdersCard } from './month-orders-amount-card'
+import { MonthRevenueCard } from './month-revenue-card'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+      <div className="flex flex-col gap-4"></div>
+
+      <div className="grid grid-cols-4 gap-4">
+        <MonthRevenueCard />
+        <MonthOrdersCard />
+        <DayOrdersAmountCard />
+        <MonthCanceledOrdersAmountCard />
+      </div>
+    </>
+  )
+}
