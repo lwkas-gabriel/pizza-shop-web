022f8e0484772423fd88a6d3a3cf068f527695d1
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-10-24 15:47:13 -0300

    feat(grafico): finalizando o grafico de produtos mais populares

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,5 +1,5 @@
 import {
-  // CartesianGrid,
+  CartesianGrid,
   Line,
   LineChart,
   ResponsiveContainer,
@@ -54,6 +54,7 @@ export function RevenueChart() {
                 })
               }
             />
+            <CartesianGrid vertical={false} className="stroke-muted" />
             <Line
               type="linear"
               strokeWidth={2}
