80e1ef3ce3768f42b091ff56deea48d915cacb2f
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-10-24 16:01:24 -0300

    feat(page): criando página de erro

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,16 +2,17 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-
+import { NotFound } from './pages/404'
+import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
-import { Dashboard } from './pages/app/dashboard/dashboard'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
