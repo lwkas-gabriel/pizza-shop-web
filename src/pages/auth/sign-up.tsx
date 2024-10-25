92f820afe219cee908cf22ef24d12f23afaae4f6
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-10-25 07:54:12 -0300

    feat(cadastro): finalizando cadastro de restaurante

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { registerRestaurant } from '@/api/register-restaurant'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -26,15 +28,24 @@ export function SignUp() {
     formState: { isSubmitting },
   } = useForm<SignUpForm>()
 
+  const { mutateAsync: registerRestautantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  })
+
   async function handleSignIn(data: SignUpForm) {
     try {
-      console.log(data)
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      await registerRestautantFn({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone,
+      })
+
       toast.success('Restaurante cadastrado com sucesso.', {
         action: {
           label: 'Login',
           onClick: () => {
-            navigate('/sign-in')
+            navigate(`/sign-in?email=${data.email}`)
           },
         },
       })
