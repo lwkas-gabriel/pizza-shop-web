c83c965ea6654ce36bad26283d959da98e739146
lwkas-gabriel <lwkasgabriel@hotmail.com>
2025-08-15 17:27:22 -0300

    fix(typo): corrigindo erro de escrita no componente de paginacao

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -60,7 +60,7 @@ export function Pagination({
             disabled={pages <= pageIndex + 1}
           >
             <ChevronRight className="h-4 w-4" />
-            <span className="sr-only">Próximo página</span>
+            <span className="sr-only">Próxima página</span>
           </Button>
 
           <Button
